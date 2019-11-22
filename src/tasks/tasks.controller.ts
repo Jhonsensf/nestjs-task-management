80946c7b617b23e2920d9d8481ab7ad3719bf9d8
jhonsensf <jose.ruiz@digital55.com>
2019-11-22 13:55:23 +0100

    first commit

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,15 +1,47 @@
-import { Controller, Get, Post, Patch, Delete, Query, Body, Param, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Query, Body, Param, UsePipes, ValidationPipe, ParseIntPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task, TaskStatus } from './task.model';
+import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks-filter-to';
 import {TaskStatusValidationPipe} from './pipes/task-status-validation.pipe'
+import { Task } from './task.entity';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService){};
 
   @Get()
+  getTasks(@Query(ValidationPipe) filterDto: GetTaskFilterDto): Promise<Task[]> {
+    return this.tasksService.getTasks(filterDto);
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task>{
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void>{
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTask(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', new TaskStatusValidationPipe) status: TaskStatus
+  ): Promise<Task> {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+
+  
+
+  /*@Get()
   getTasks(@Query(ValidationPipe) filterDto: GetTaskFilterDto): Task[] {
     console.log(filterDto);
     if (Object.keys(filterDto).length){
@@ -42,5 +74,5 @@ export class TasksController {
     @Body('status', new TaskStatusValidationPipe) status: TaskStatus
   ){
     return this.tasksService.updateTaskStatus(id, status);
-  }
+  }*/
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,36 +1,20 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model'
-import * as uuid from 'uuid'
+import { TaskStatus } from './task-status.enum'
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks-filter-to';
+import { TaskRepository } from './task.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
 
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = []
+  constructor(
+    @InjectRepository(TaskRepository)
+    private taskRepository: TaskRepository
+  ){}
 
-  getAllTasks(){
-    return this.tasks;
-  }
-
-  getAllTasksWithFitler(filterDto: GetTaskFilterDto){
-    const {status, search } = filterDto;
-    let tasks = this.getAllTasks();
-    
-    if (status) {
-      tasks = tasks.filter(task => task.status === status)
-    }
-
-    if (search) {
-      tasks = tasks.filter(task => 
-        task.title.includes(search) || 
-        task.description.includes(search)
-      );
-    }
-    return tasks;
-  }
-
-  getTaskById(id: string): Task {
-    const found = this.tasks.find(task => task.id === id);
+  async getTaskById(id: number): Promise<Task>{
+    const found = await this.taskRepository.findOne(id); 
 
     if (!found){
       throw new NotFoundException(`Tarea con id "${id}" no encontrada`);
@@ -38,26 +22,27 @@ export class TasksService {
     return found
   }
 
-  createTask(createTaskDto: CreateTaskDto): Task{
-    const {title, description} = createTaskDto;
-    const task: Task ={
-      id: uuid(),
-      title,
-      description,
-      status: TaskStatus.OPEN
-    };
-    this.tasks.push(task);
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task>{
+    return this.taskRepository.createTask(createTaskDto)
+  }
+  
+
+  async deleteTask(id: number): Promise<void>{
+    const result = await this.taskRepository.delete(id);
+
+    if (result.affected === 0){ 
+      throw new NotFoundException(`Tarea con id "${id}" no encontrada`);
+    }
+  }
+
+  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task>{
+    const task = await this.getTaskById(id);
+    task.status = status;
+    await task.save();
     return task;
   }
 
-  deleteTask(id: string): void{
-    const found = this.getTaskById(id);
-    this.tasks = this.tasks.filter(task => task.id !== found.id);
-  }
-
-  updateTaskStatus(id: string, status: TaskStatus): Task{
-    const task = this.getTaskById(id);
-    task.status = status;
-    return task
+  async getTasks(filterDto: GetTaskFilterDto): Promise<Task[]>{
+    return this.taskRepository.getTasks(filterDto)
   }
 }
