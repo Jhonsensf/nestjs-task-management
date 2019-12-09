0fd3bcb46058ab658efc6ec0680976c96092ce18
jhonsensf <jose.ruiz@digital55.com>
2019-12-09 17:48:54 +0100

    add validations

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
@@ -9,7 +9,7 @@ export class AuthController {
   ){
   }
   @Post('/singup')
-  singUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+  singUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.singUp(authCredentialsDto)
   }
 }
